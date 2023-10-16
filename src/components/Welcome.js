2cf77cd54f3aa5bad194cc64e2446b9247398032
Shakhawat0989 <shakhawat15-2335@diu.edu.bd>
2023-10-16 22:11:04 +0600

    React State

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 
 class good extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            fruits:[
+                { name: 'apple',weight:'40gm'},
+                { name: 'Mango',weight:'10gm'},
+                { name: 'Orange',weight:'30gm'},
+                { name: 'Banana',weight:'20gm'},
+            ]
+        }
+    }
+
+
     render(){
-        return <h1>Hello Bangladesh</h1>
+        return(
+            <div>
+                <h1>Fruit Name is { this.state.fruits[3].name } and weight is {this.state.fruits[3].weight}</h1>
+                <h1>Fruit Name is { this.state.fruits[2].name } and weight is {this.state.fruits[2].weight}</h1>
+                <h1>Fruit Name is { this.state.fruits[1].name } and weight is {this.state.fruits[1].weight}</h1>
+                <h1>Fruit Name is { this.state.fruits[0].name } and weight is {this.state.fruits[0].weight}</h1>
+            </div>
+        )
     }
 }
 
